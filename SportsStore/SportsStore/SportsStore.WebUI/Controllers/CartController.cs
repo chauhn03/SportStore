2b24fb48fd344e6fcf6e6ab5654fea74f7a205f5
chauhn.it@gmail.com <chauhn.it@gmail.com@8cda0039-e6d3-cfb9-c726-e4171ab3fdef>
2014-04-28 01:37:34 +0000

    processor an order

---
+++ SportsStore/SportsStore/SportsStore.WebUI/Controllers/CartController.cs
@@ -0,0 +1,63 @@
+﻿using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Providers.Entities;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private IProductRepository repository;
+
+        public CartController(IProductRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public ViewResult Index(Cart cart, string returnUrl)
+        {
+            return this.View(new CartIndexViewModel
+            {
+                Cart = cart,
+                ReturnUrl = returnUrl
+            });
+        }
+
+        public RedirectToRouteResult AddToCart(Cart cart, int productId, string returnUrl)
+        {
+            Product product = this.repository.Products.SingleOrDefault(p => p.ProductId == productId);
+            if (product != null)
+            {
+                cart.AddItem(product, 1);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToRouteResult RemoveFromCart(Cart cart, int productId, string returnUrl)
+        {
+            Product product = this.repository.Products.SingleOrDefault(p => p.ProductId == productId);
+            if (product != null)
+            {
+                cart.RemoveItem(product);
+            }
+
+            return this.RedirectToAction("Index", new { returnUrl });
+        }
+
+        public PartialViewResult Summary(Cart cart)
+        {
+            return this.PartialView(cart);
+        }
+
+        public ViewResult Checkout()
+        {
+            return this.View(new ShippingDetails());
+        }
+    }
+}
---
+++ SportsStore/SportsStore/SportsStore.WebUI/HtmlHelpers/PagingHelpers.cs
@@ -0,0 +1,31 @@
+﻿using SportsStore.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.HtmlHelpers
+{
+    public static class PagingHelpers
+    {
+        public static MvcHtmlString PageLinks(this HtmlHelper html, PagingInfo pagingInfo, Func<int, string> pageUrl)
+        {
+            StringBuilder htmlStringBuilder = new StringBuilder();
+            for (int index = 1; index <= pagingInfo.TotalPages; index++)
+            {
+                TagBuilder tagBuilder = new TagBuilder("a");
+                tagBuilder.MergeAttribute("href", pageUrl(index));
+                tagBuilder.InnerHtml = index.ToString();
+                if (index == pagingInfo.CurrentPage)
+                {
+                    tagBuilder.AddCssClass("selected");
+                }
+
+                htmlStringBuilder.Append(tagBuilder.ToString() + "  ");
+            }
+            return MvcHtmlString.Create(htmlStringBuilder.ToString());
+        }
+    }
+}
