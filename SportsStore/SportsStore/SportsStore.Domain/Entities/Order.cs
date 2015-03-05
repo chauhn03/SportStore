d1573dc09359d001fb2a983b71b12655c6051e9a
chauhn.it@gmail.com <chauhn.it@gmail.com@8cda0039-e6d3-cfb9-c726-e4171ab3fdef>
2015-03-05 11:02:45 +0000

    add dummy data to test orders

--- SportsStore/SportsStore/SportsStore.Domain/Entities/Order.cs
+++ SportsStore/SportsStore/SportsStore.Domain/Entities/Order.cs
@@ -1,15 +1,42 @@
 ﻿
+using System;
 using System.Collections.Generic;
 namespace SportsStore.Domain.Entities
 {
+    public enum OrderStatus
+    {
+        New,
+        Done
+    }
+
     public class Order
     {
+        public Order()
+        {
+            this.Status = (int)OrderStatus.New;
+            this.OrderDate = DateTime.Now;
+        }
+
         public int OrderId { get; set; }
 
+        public string OrderNo { get; set; }
+
         public int CustomerId { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+
+        public string Phone { get; set; }
 
         public List<OrderDetail> OrderDetails { get; set; }
 
         public decimal Total { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        public int Status { get; set; }
     }
 }
--- SportsStore/SportsStore/SportsStore.Repository/Fake/FakeOrderRepository.cs
+++ SportsStore/SportsStore/SportsStore.Repository/Fake/FakeOrderRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using SportsStore.Domain.Entities;
 using SportsStore.Repository.Abstract;
@@ -37,7 +38,108 @@ namespace SportsStore.Repository.Fake
         {
             Order order = this.GetById(entity.OrderId);
             order.CustomerId = entity.CustomerId;
-            order.Total = entity.Total;            
+            order.Total = entity.Total;
+        }
+
+        private void GenerateDummyData()
+        {
+            this.orders = new List<Order>();
+            this.orders.Add(new Order
+            {
+                OrderId = 1,
+                OrderNo = "Order 1",
+                Address = "123 Lê Thánh Tôn",                
+                CustomerName = "Nguyễn Văn A",
+                Email = "nguyenvana@gmail.com",
+                OrderDate = DateTime.Now,
+                Phone = "09332345678",
+                Total = 12000
+            });
+
+            this.orders.Add(new Order
+            {
+                OrderId = 2,
+                OrderNo = "Order 2",
+                Address = "456 Mạc Đỉnh Chi",
+                CustomerName = "Lê Thị B",
+                Email = "lethib@gmail.com",
+                OrderDate = DateTime.Now,
+                Phone = "0903911228",
+                Total = 36000
+            });
+
+
+            this.orders.Add(new Order
+            {
+                OrderId = 3,
+                OrderNo = "Order 3",
+                Address = "179 Tôn Thất Tùng",
+                CustomerName = "Lê Văn C",
+                Email = "levanc@gmail.com",
+                OrderDate = DateTime.Now,
+                Phone = "098355428",
+                Total = 25000
+            });
+
+            this.orders.Add(new Order
+            {
+                OrderId = 4,
+                OrderNo = "Order 4",
+                Address = "259 Trần Quang Diệu",
+                CustomerName = "Trần Minh D",
+                Email = "tranminhd@gmail.com",
+                OrderDate = DateTime.Now,
+                Phone = "01662949750",
+                Total = 42000
+            });
+
+            this.orders.Add(new Order
+            {
+                OrderId = 5,
+                OrderNo = "Order 5",
+                Address = "256 Trần Não",
+                CustomerName = "Phan Minh E",
+                Email = "phanminhe@gmail.com",
+                OrderDate = DateTime.Now,
+                Phone = "01687459234",
+                Total = 56000
+            });
+
+            this.orders.Add(new Order
+            {
+                OrderId = 6,
+                OrderNo = "Order 6",
+                Address = "76 Phan Đăng Lưu",
+                CustomerName = "Lê Huy F",
+                Email = "lehuyf@gmail.com",
+                OrderDate = DateTime.Now,
+                Phone = "0906478954",
+                Total = 45000
+            });
+
+            this.orders.Add(new Order
+            {
+                OrderId = 7,
+                OrderNo = "Order 7",
+                Address = "85 Phan Huy Ích",
+                CustomerName = "Nguyễn Thị H",
+                Email = "nguyenthih@gmail.com",
+                OrderDate = DateTime.Now,
+                Phone = "0902945678",
+                Total = 32000
+            });
+
+            this.orders.Add(new Order
+            {
+                OrderId = 8,
+                OrderNo = "Order 8",
+                Address = "65 Lê Quang Định",
+                CustomerName = "Quách Thị K",
+                Email = "quachthik@gmail.com",
+                OrderDate = DateTime.Now,
+                Phone = "09029985124",
+                Total = 52000
+            });
         }
     }
 }
